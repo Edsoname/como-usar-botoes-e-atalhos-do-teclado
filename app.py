f47b73e42bb24d736ad81beaac5abbed52496543
codigo de automação teste 1 <edson451.ea@gmail.com>
2024-02-20 00:29:11 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+#como usar botões e atalhos do teclado
+import pyautogui
+from time import sleep
+#navegar e clicar no campo email
+pyautogui.click(1053,272,duration=2)
+sleep(1)
+#Digitar o email   
+pyautogui.typewrite('edson451.ea@gmail.com')
+sleep(1)
+#apertar tab
+pyautogui.press('tab')
+sleep(1)
+#digitar minha senha
+pyautogui.typewrite('verdao1914')
+sleep(1)
+#apertar tab
+pyautogui.press('tab')
+
+pyautogui.click(1018,480,duration=1)
+
+
+
+# apertar space
+
+
